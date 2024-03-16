36e75d58dae8cbac0183e965765c4472339fc452
ToffanTee <tonyetoffan@gmail.com>
2024-03-16 14:26:33 -0700

    Update app

--- src/components/BlogComponents/BlogCard.js
+++ src/components/BlogComponents/BlogCard.js
@@ -4,7 +4,7 @@ import BlogImage from "../../assets/Blog/happy-client-with-their-box-delivered.j
 const BlogCard = ({ title, content }) => {
   return (
     <div className="col-span-1">
-      <div className="bg-white rounded-lg shadow-md p-4">
+      <div className="blog_card">
         <img src={BlogImage} alt="Blog Post 1" className="rounded-md mb-4" />
         <h2 className="text-xl font-semibold mb-2">{title}</h2>
         {/* <p className="text-gray-700">{content.substring(1, 100)}</p> */}
