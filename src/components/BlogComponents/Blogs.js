4fe539989ef204a7846be5d491b8bd74e647a36f
ToffanTee <tonyetoffan@gmail.com>
2024-04-30 10:22:47 -0400

    Update laoding state on blogs

--- src/components/BlogComponents/Blogs.js
+++ src/components/BlogComponents/Blogs.js
@@ -3,7 +3,8 @@ import { useGetAllBlogsMutation } from "../../lib/APIs/blogAPI";
 import BlogCard from "./BlogCard";
 
 const Blogs = () => {
-  const [getAllBlogs, { data, isError, error }] = useGetAllBlogsMutation();
+  const [getAllBlogs, { data, isLoading, isError, error }] =
+    useGetAllBlogsMutation();
 
   useEffect(() => {
     getAllBlogs();
@@ -12,16 +13,20 @@ const Blogs = () => {
   return (
     <div className="container mx-auto py-8 mt-20">
       <h1 className="text-3xl text-center font-bold mb-8">Latest Blog Posts</h1>
-      <div className="grid  grid-cols-1 sm:grid-cols-3 gap-8">
-        {isError && (
-          <div class="alert alert-danger" role="alert">
-            {error?.error || "Something went wrong"}
-          </div>
-        )}
-        {data?.map((blog) => {
-          return <BlogCard key={blog._id} {...blog} />;
-        })}
-      </div>
+      {isLoading ? (
+        <div className="text-3xl text-center font-bold">Loading...</div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+          {isError && (
+            <div className="alert alert-danger" role="alert">
+              {error?.error || "Something went wrong"}
+            </div>
+          )}
+          {data?.map((blog) => {
+            return <BlogCard key={blog._id} {...blog} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,46 +1,7 @@
-import { useState, useEffect } from "react";
 import Blogs from "../components/BlogComponents/Blogs";
 
 const BlogPage = () => {
-  const [isLoading, setIsLoading] = useState(true); // Initial loading state
-  const [blogs, setBlogs] = useState([]); // Array to store fetched blogs
-
-  useEffect(() => {
-    const fetchBlogs = async () => {
-      setIsLoading(true); // Set loading to true before fetching
-
-      try {
-        const response = await fetch("/api/blogs"); // Replace with your API endpoint
-        const data = await response.json();
-        setBlogs(data);
-      } catch (error) {
-        console.error("Error fetching blogs:", error);
-      } finally {
-        setIsLoading(false); // Set loading to false after fetching (or on error)
-      }
-    };
-
-    fetchBlogs();
-  }, []); // Run effect only once on component mount
-
-  return (
-    <div>
-      {isLoading ? (
-        // Display loading indicator here (e.g., spinner, skeleton screen)
-        <div className="loading">Loading...</div>
-      ) : (
-        <Blogs blogs={blogs} />
-      )}
-    </div>
-  );
+  return <Blogs />;
 };
 
 export default BlogPage;
-
-// import Blogs from "../components/BlogComponents/Blogs";
-
-// const BlogPage = () => {
-//   return <Blogs />;
-// };
-
-// export default BlogPage;
